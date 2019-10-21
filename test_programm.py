3613ee4ec028880a389baf4645d05bb798d202d7
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2019-10-21 14:52:19 +0200

    Upload test_programm on new branch

--- test_programm.py
+++ test_programm.py
@@ -1,13 +1,19 @@
 import nasa_hls
+#
+# available_tiles = nasa_hls.get_available_tiles_from_url()
+# print("Total number of tiles: ", len(available_tiles))
+# print("First tiles: ", available_tiles[:3])
+# print("Last tiles: ", available_tiles[-3:])
+#
+# print(type(available_tiles))
 
-available_tiles = nasa_hls.get_available_tiles_from_url()
-print("Total number of tiles: ", len(available_tiles))
-print("First tiles: ", available_tiles[:3])
-print("Last tiles: ", available_tiles[-3:])
-
-print(type(available_tiles))
-
-
+# returns list
+urls_datasets = nasa_hls.get_available_datasets(products=["L30", "S30"],
+                                                years=[2018],
+                                                tiles=["32UNU", "32UPU"])  # here to be prompted a shape
+print("Number of datasets: ", len(urls_datasets))
+print("First datasets:\n -", "\n - ".join(urls_datasets[:3]))
+print("Last datasets:\n -", "\n - ".join(urls_datasets[-3:]))
 
 
 
