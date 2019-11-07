f48a95c18a9ab33199155ac2401174519216d3ea
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2019-11-07 17:38:05 +0100

    [spatial_querying] Final changes on functions get_available_datasets_from_tiles, get_required_tiles_from_utm

--- nasa_hls/spatial_querying.py
+++ nasa_hls/spatial_querying.py
@@ -1,10 +1,19 @@
+import urllib
+from os import path
 from pathlib import Path
+
 import geopandas as gp
+
 import nasa_hls
+
 
 def download_kml(dst):
     """
     Download the necessary .kml-file
+
+    :param dst: desired destination
+
+    :return: destination of the .kml-file
     """
 
     print("'y'  to download the ~ 100MB Files")
@@ -13,23 +22,22 @@ def download_kml(dst):
 
     choice = input("What would you like to do?")
 
-
     while choice != "q":
-        
+
         if choice == "y":
             if path.exists(dst) == False:
-                src = ("https://hls.gsfc.nasa.gov/wp-content/uploads/2016/03/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml")
+                src = (
+                    "https://hls.gsfc.nasa.gov/wp-content/uploads/2016/03/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml")
                 urllib.request.urlretrieve(src, dst)
                 return dst
             else:
                 print("sorry, this path already exists")
-                break
-        
+                return dst
+
         elif choice == "q":
-            break   
+            break
         else:
             choice = input("Sorry I did't quite get it")
-
 
 
 def get_required_tiles_from_utm(path_to_utm_file="/home/aleko-kon/projects/geo419/nasa-hls/ignored/UTM_tiles.kml",
@@ -68,6 +76,6 @@ def get_available_datasets_from_tiles(products=["S30"],
                                       user_shape="/home/aleko-kon/projects/geo419/nasa-hls/ignored/user_shape/dummy_region_europe.shp"):
     # retrieve required tiles from the function above
     tiles = get_required_tiles_from_utm(user_shape=user_shape)
-    datasets = nasa_hls.get_available_datasets(products=products, years=years, tiles=tiles, return_list=True)
+    datasets = nasa_hls.get_available_datasets(products=products, years=years, tiles=tiles, return_list=False)
 
     return datasets
