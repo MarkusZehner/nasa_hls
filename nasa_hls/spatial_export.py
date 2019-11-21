a8285baac969724b34fd9c1c0a386afa23f01d1b
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2019-11-21 11:58:04 +0100

    [spatial_export] initialisation and John's entries.

---
+++ nasa_hls/spatial_export.py
@@ -0,0 +1,19 @@
+import rasterio
+import os
+
+#auxpath
+path = os.path.join(os.path.expanduser('~'), '.nasa_hls')
+
+path = '/media/aleko-kon/Daten/Geodaten/Master/GEO419'
+
+vrt = os.path.join(path, 'auxdata', 'test.vrt')
+
+
+shp_path = '/media/aleko-kon/Daten/Geodaten/Master/GEO419/auxdata/user_shape/dummy_region.shp/'
+
+with Vector(shp) as site:
+    dem_autoload([site], 'SRTM 1Sec HGT', vrt=vrt)
+
+options = gdal.WarpOptions(format='GTiff')
+
+gdal.Warp(destNameOrDestDS='test.tif', srcDSOrSrcDSTab='test.vrt', options=options)
