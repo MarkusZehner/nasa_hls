0527fca400ea44bd715aef320a9d31d6d58f8d04
RobinKohrs <robin.kohrs@gmx.de>
2020-01-19 18:58:30 +0100

    [gdal_operations] convertion from vrt to final (uncropped) tiff. The projection is wrong and I don't know how to fix it

--- nasa_hls/gdal_operations.py
+++ nasa_hls/gdal_operations.py
@@ -20,6 +20,8 @@ def main():
     else:
         pass
 
+    
+
     # here the user shapes should be placed
     path_data = os.path.join(os.path.expanduser('~'), '.nasa_hls', '.data')
     if not os.path.exists(path_data):
@@ -44,13 +46,12 @@ def main():
     hdf_dir = "/home/robin/python_projects/data/nasa_hls/hdf_tiles/"
     # "/home/robin/python_projects/data/nasa_hls/hdf_tiles"
     hdf_path = list(glob.glob(os.path.join(hdf_dir, '*.hdf')))
-    print(hdf_path)
 
     # make list with gdal datasets
-    gdal_datasets = []
-    for i in hdf_path:
-        dataset = gdal.Open(i)
-        gdal_datasets.append(dataset)
+    # gdal_datasets = []
+    # for i in hdf_path:
+    #     dataset = gdal.Open(i)
+    #     gdal_datasets.append(dataset)
 
     # build vrt with all from all the -hdf files specified
     # print("making vrt")
@@ -67,19 +68,29 @@ def main():
     # set vrt-options. Don't know why, but on the command line requires different projections
     vrtoptions = gdal.BuildVRTOptions(allowProjectionDifference=True, separate=True)
     vrt_path = os.path.join(os.path.expanduser('~'), '.nasa_hls', '.data', "final.vrt")
+    print("the path where your vrt and final tiff will be is: \n {path}".format(path = vrt_path))
 
     # make vrt 
     final_vrt = gdal.BuildVRT(vrt_path, hdf_path, options=vrtoptions)
+
+    #get projection
+    projection = final_vrt.GetProjection()
+
+    # reproject vrt
+    final_vrt = gdal.Warp(vrt_path, final_vrt, dstSRS=projection)
     final_vrt = None
 
 
     # convert vrt to tiff
-    final_tif = gdal.Translate(os.path.join(path_data + "/" + "final.tiff"), final_vrt)
-    print("final tif created \n\n")
-    print("Now proceed to clipping")
+    final_tif = gdal.Translate(os.path.join(path_data + "/" + "final2.tiff"), vrt_path, projWinSRS=projection)
+    print("\nfinal tif created \n")
+    print("Now proceed to clipping \n")
+
     # use Johns spatialist to clip
     # with Vector(shp_path) as site:
     #     dem_autoload([site], 'SRTM 1Sec HGT', vrt=vrt)
+
+    return None
 
 if __name__ == "__main__":
     main()
