44c02b89d5c77c4677123b40efbbaed1648ec723
konstantinschellenberg <konstantin.schellenberg@posteo.de>
2020-01-23 21:31:33 +0100

    [download_tiles.py] more flexible use of make_tiles_dataset. Printing of input status. Trying to implement error raising if user shape misses. Started implementing option for time span input as parameter.

--- nasa_hls/download_tiles.py
+++ nasa_hls/download_tiles.py
@@ -11,6 +11,7 @@ path_data_lin_robin = os.path.join(os.path.expanduser('~'), 'python_projects', '
 
 path_auxil = os.path.join(os.path.expanduser('~'), '.nasa_hls', '.auxdata' + os.sep)
 
+
 def download_kml():
     """
     Download the necessary .kml-file
@@ -30,52 +31,50 @@ def download_kml():
         print(f"UTM tiles already successfully downloaded to:\n", path, "\n")
     return path
 
+
 def get_available_datasets_from_tiles(products=None,
                                       years=None,
-                                      user_shape=None,
-                                      utm_tiles=None):
+                                      shape=None):
     """
     Calls the Nasa's world-covering UTM.kml file being stored. Do this manually by calling function 'download_kml'.
 
-    :param user_shape -> shape of the region of interest (ROI)
+    :param shape -> shape of the region of interest (ROI)
     :param years -> required years to be checked for
     :param products -> either L30 or S30, the Landsat 8 or Sentinel 2 product, respectively
-    :param utm_tiles -> if required: add a custom utm grid (must be in .kml format and must comply with nasa's file
 
     :return: list of tile name [str of 5 digits starting with two numbers] which geographically intersect the user
     shape and the UTM tiles.
     """
 
     # define defaults
-    if user_shape is None:
-        user_shape = path_data_lin_konsti + "dummy_region.shp"
+    if shape is None:
+        print("no shape given") # raise error here
     if products is None:
         products = ["S30"]
     if years is None:
         years = [2018]
-    if utm_tiles is None:
-        gp.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw' # Enable fiona driver, read in utm grid from kml
-        utm_tiles = gp.read_file(download_kml(), driver='KML')
 
-    # convert user_polygon into Gdf
-    user_polygon = gp.GeoDataFrame.from_file(user_shape)
+    gp.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'  # Enable fiona driver, read in utm grid from kml
+    utm_tiles = gp.read_file(download_kml(), driver='KML')
 
-    # perform intersection
+    # convert user_polygon into Gdf -> perform intersection
+    user_polygon = gp.GeoDataFrame.from_file(shape)
     match = gp.sjoin(user_polygon, utm_tiles, how="inner", op='intersects')
 
     # write UTM-codes in list
     tiles = match["Name"].tolist()
 
+    print("getting available datasets . . .")
     datasets = get_available_datasets(products=products, years=years, tiles=tiles, return_list=False)
 
     return datasets
 
 
-def make_tiles_dataset(shape=path_data_lin_konsti,
-                       date=2018,
-                       start_date = "",
-                       end_date = ""):
-
+def make_tiles_dataset(shape=None,
+                       products=None,
+                       date="2018-01-08",
+                       start_date=None,
+                       end_date=None):
     """
     :param: shape, date, start_date, end_date, product
     :return: dataset(s). contains date specific tiles in the spatial extent of the input shape.
@@ -85,16 +84,32 @@ def make_tiles_dataset(shape=path_data_lin_konsti,
     1. df -> when there is only a single date
     2. list of df -> when time span is specified (iterable)
     """
+    if shape is None:
+        print("please specify a shape..!") # raise an error here!
 
-    df = get_available_datasets_from_tiles()
+    if products is None:
+        products = ["S30"]
 
+    if date:
+        print(f"single date information: ", date)
+    if start_date:
+        print(f"starting date: ", start_date)
+    if end_date:
+        print(f"end date: ", end_date)
 
-    # download_kml() -> pfad
-    # get_required_tiles_from_utm() -> list(tiles_no)
-    # get_available_datasets_from_tiles(products=, years=)
-    # extract_date(date=, start_date=, end_date=)
-    #
-    # return datasets # welches heruntergeladen werden soll -> download_tiles()
+    datasets = None
+    yyyy = [date[0:4]]
+    print(f"year: ", yyyy)
+
+    # SINGLE YEAR INPUT parse year from date input
+    if not start_date:
+        df = get_available_datasets_from_tiles(products=products, shape=shape, years=yyyy)
+        datasets = extract_date(df, date)
+
+    # TIME SPAN INPUT
+    # yet to be developed!
+
+    return datasets
 
 # def download_tiles():
 #     """
@@ -118,12 +133,12 @@ def show_available_dates(df):
     print(type(df))
     df_sorted = df.sort_values("date")
     df_grouped = df_sorted.groupby(['date']).count()
-    df_selected = df_grouped.iloc[:,0:1]
+    df_selected = df_grouped.iloc[:, 0:1]
 
     return df_selected
 
 
-def extract_date(df, date = "2018-01-01"):
+def extract_date(df, date="2018-01-01"):
     """
     date: date in the format "yyyy-mm-dd"
     df: dataframe-object returned by the "get_available_datasets_from_tiles"-function
@@ -141,13 +156,6 @@ def extract_date(df, date = "2018-01-01"):
         return None
     else:
         df = df.loc[date]
-        print("\n \n There are {nrows} scenes available for the specified date and location".format(nrows = df.shape[0]))
+        print("\n \n There are {nrows} scenes available for the specified date and location".format(nrows=df.shape[0]))
 
     return df
-
-
-
-    
-
-
-
--- nasa_hls/notes.md
+++ nasa_hls/notes.md
@@ -1,8 +1,18 @@
+# Notes
+#### quicknotes
+
+make_tiles_dataset funktioniert soweit! Hast du ne Ahnung, wie man errors raisen kann (
+in dem Fall, dann, wenn keins oder ein invalides shape übergeben wird)? Wäre richtig geil!
+
 @robin: Mit PyCharm Professional kannst du einfach ein Notebook in der repo dir 
 erstellen (so wie ich jetzt test.ipynb). Dann einfach die Datei in PCh öffnen und, rechts
 erscheint die Code Chunk Struktur :D So können wir auch den Report machen, denke ich. Fände 
 ich besser als in Rmd... Es sein denn, es will tatsächliche Seiten haben. Dann würde ich 
 Rmd mit Python chunks verwenden. (?)
+
+@robin: Wir brauchen noch eine verbesserte Tabelle, die aus extract_date kommt... Also eine,
+mit der wir downloac_batch aufrufen können :) Und, dass sie start_date & end_date handeln kann
+wie die beiden parameter in make_tiles_dataset übergeben werden!
 
 
 ## Get date-metadate from file
