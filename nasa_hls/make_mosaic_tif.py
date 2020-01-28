da056f6ea32caaa66e2ab73ae85201d32b16662a
RobinKohrs <robin.kohrs@gmx.de>
2020-01-28 21:34:40 +0100

    [make_mosaic_tif] basic make_mosaic_tif-function. Still without splitting up the dates

--- nasa_hls/make_mosaic_tif.py
+++ nasa_hls/make_mosaic_tif.py
@@ -13,4 +13,15 @@ from nasa_hls.download_tiles import path_data_lin_robin
 from nasa_hls.download_tiles import path_data_lin_konsti
 
 # make mosaic function
-def make_mosaic_tif(srcdir = path_data_lin_robin +"hdf/", dstdir = path_data_lin_robin + "mosaic/"):
+def make_mosaic_tif(srcdir = path_data_lin_robin + "hdf/", dstdir = path_data_lin_robin + "mosaic/"):
+    hdf_files_list = list(glob.glob(srcdir + '*.hdf'))
+
+    vrt_path = os.path.join(path_data_lin_robin, "mosaic", "mosaic.vrt")
+    # make vrt 
+    final_vrt = gdal.BuildVRT(vrt_path, hdf_files_list)
+    final_vrt = None
+
+    # make tif
+    final_tif = gdal.Translate(os.path.join(path_data_lin_robin + "mosaic/" + "final.tiff"), vrt_path)
+
+    return final_vrt
