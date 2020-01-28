e705384362162f8e770911ed397f2ea392ca6974
RobinKohrs <robin.kohrs@gmx.de>
2020-01-28 19:49:19 +0100

    [download_tiles] datetime-fun

--- nasa_hls/download_tiles.py
+++ nasa_hls/download_tiles.py
@@ -3,6 +3,8 @@ import urllib
 import geopandas as gp
 from nasa_hls.utils import get_available_datasets
 import pandas as pd
+import numpy as np
+import datetime
 
 path_data_win_konsti = os.path.join("D:", os.sep, "Geodaten", "#Jupiter", "GEO419", "data" + os.sep)
 path_data_lin_konsti = os.path.join(os.path.expanduser('~'), 'Dokumente', 'nasa_hls', 'data' + os.sep)
@@ -60,8 +62,9 @@ def get_available_datasets_from_shape(products=None,
     # write UTM-codes in list
     tiles = match["Name"].tolist()
 
-    print("getting available datasets . . .")
+    print("\ngetting available datasets . . .")
     datasets = get_available_datasets(products=products, years=years, tiles=tiles, return_list=False)
+    
 
     return datasets
 
@@ -82,60 +85,109 @@ def make_tiles_dataset(shape=None,
     2. list of df -> when time span is specified (iterable)
     """
 
-    # TODO: convert date to datetime Timestamp (numpy)
+    try:
+     # convert given dates to pd.timestampe
+        if date:
+            date_timestamp = pd.Timestamp(date).date()
+        if start_date:
+            start_date = pd.Timestamp(start_date).date()
+        if end_date:
+            end_date = pd.Timestamp(end_date).date()
+    except ValueError:
+        print("provide a string in the format 'yyyy-mm-dd'") # if this error is raised, all the rest should not even be evaluated
 
-    while True:
-        try:
-            shape = gp.read_file(shape)
-            print("valid shape, process continues")
+    # still needs a little description about whats happening here
+    # while True:
+    #     try:
 
-            if products is None:
-                products = ["S30"]
-            if date:
-                print(f"single date information: ", date)
-            if start_date:
-                print(f"starting date: ", start_date)
-            if end_date:
-                print(f"end date: ", end_date)
+    shape = gp.read_file(shape)
+    print("valid shape, process continues\n")
 
-            # TODO: years = extract year from timedate object
+    if products is None:
+        products = ["S30"]
+    if date:
+        print("single date: {date}\n ".format(date = date))
+    if start_date:
+        print("starting date: {date}\n ".format(date = start_date))
+    if end_date:
+        print("end date: {date}\n ".format(date = end_date))
+    if year:
+        print("year: {year}".format(year = year))
 
-            yyyy = [date[0:4]]
 
-            df = get_available_datasets_from_shape(products=products, shape=shape, years=yyyy)
-            # SPLIT DATAFRAME AS USER DATE INPUT  . . .
-            dictionary = dates_to_dict(df)
+    # get the year (any year!!) in the format that get_available_datasets needs
+    # we need a year to trigger the function of ben mack
+    # we just split up the data afterwards
+    if date is not None:
+        # timestamp = pd.to_datetime(date)
+        # year_int = timestamp.year
+        yyyy = [date_timestamp.year]
 
-            # TODO. TRY-EXEPT (low prio)
+    else:
+        if start_date is not None:
+            yyyy = [start_date.year]
+        elif end_date is not None:
+            yyyy = [end_date.year]
+        else:
+            yyyy = [year]
+            
+    # for "debugging" purposes
+    # print("""the function reached this point and the parameter provided to get_available_datasets_from_shape are:
+    # \n\n {products}, \n\n {shape}, "\n\n", {years}""".format(products = products, shape = shape, years = yyyy))
 
-            # # Ein Tag
-            # if date is not None:
-            #     # TODO. nur ein Tag
-            #
-            # # Zeiraum
-            # elif start_date is not None or end_date is not None:
-            #     # TODO: only start_date -> get dict for rest of year
-            #     # TODO: only end_date -> get dict for beginning of year
-            #
-            #     # TODO: Start_date
-            #     if:
-            #         # T
-            #
-            #     # TODO: End_date
-            #     if:
-            #         #
-            #
-            # # year
-            # elif year is not None:
-            #     # get for whole year
-            #
-            break
 
-        except:
-            print("something is wrong with your shapefile. This will not work\n\n")
-            return None
+    df = get_available_datasets_from_shape(products=products, shape=shape, years=yyyy)
+    # SPLIT DATAFRAME AS USER DATE INPUT  . . .
+    dictionary = dates_to_dict(df)
 
-    return dictionary
+   
+    # 1 Dataframe is 1 Date and can consist of up to 4 rows(Scenes)!!!
+    # make list with all the dataframe the user wants
+    dataframes = []
+    
+
+    # # Ein Tag
+    if date is not None:
+        df_single_date = dictionary[date_timestamp]
+        dataframes.append(df_single_date)
+
+        
+    # # Zeiraum
+    elif start_date is not None and end_date is not None:
+        for key in dictionary.keys():
+            if key >= start_date and key <= end_date:
+                dataframes.append(dictionary[key])
+            else:
+                pass
+
+    # Zeitraum mit nur Ende oder Anfang 
+    elif start_date is not None or end_date is not None:
+        
+        
+        if start_date is not None and end_date is None:
+            for key in dictionary.keys():
+                if key >= start_date:
+                    dataframes.append(dictionary[key])
+        elif start_date is None and end_date is not None:
+            for key in dictionary.keys():
+                if key <= end_date:
+                    dataframes.append(dictionary[key])
+
+    # # year
+    elif year is not None:
+        for key in dictionary.keys():
+            dataframes.append(dictionary[key])
+    
+    else:
+        print("Something is wrong with your dates")
+    
+        #break
+
+        #except:
+        #    print("something is wrong with your shapefile. This will not work\n\n")
+        #    return None
+
+    return dataframes
 
 def download_tiles(datasets = None,
                    dstdir = path_data_win_konsti + "hdf/",
@@ -175,9 +227,16 @@ def dates_to_dict(df):
 
     # make numpy array of unique dates
     unique_dates = df_sorted.date.unique()
+    dates_ls = []
+
+    # instead of using numpy.datetime64 use pandas.Timestamp
+    for i in unique_dates:
+        i = pd.Timestamp(i)
+        i = i.date()
+        dates_ls.append(i)
 
     # create dictionary with keys being the unique dates
-    dataframe_dict = {date: pd.DataFrame for date in unique_dates}
+    dataframe_dict = {date: pd.DataFrame for date in dates_ls}
 
     # add rows of orignial dataframe as values
     for key in dataframe_dict.keys():
